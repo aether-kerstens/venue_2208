fba4d8a574d459f5303936b834e75b5c5663f333
Aether Kerstens <philippekerstens@gmail.com>
2022-08-24 09:46:43 -0700

    Save venue.rb

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,16 @@
 class Venue
-end
+  attr_reader :name, :capacity, :patrons
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+  def add_patron
+    venue.patrons[] = patron;
+      venue.size += 1
+    end
+  end
+
+  def yell_at_patrons
+    patrons.upcase
+  end
